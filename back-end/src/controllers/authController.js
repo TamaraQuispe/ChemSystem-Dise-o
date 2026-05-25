c8faa73ef557f4dc7e6a66a7e61c7b4e6cb17999
TamaraQuispe <U23204737@utp.edu.pe>
2026-05-25 18:28:03 -0500

    modulo de simulador terminado

---
+++ back-end/src/controllers/authController.js
@@ -0,0 +1,25 @@
+const authService = require('../services/authService');
+
+const register = async (req, res, next) => {
+  try {
+    const result = await authService.register(req.body);
+    res.status(201).json({ success: true, data: result });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const login = async (req, res, next) => {
+  try {
+    const result = await authService.login(req.body);
+    res.json({ success: true, data: result });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const me = async (req, res) => {
+  res.json({ success: true, data: { user: req.user.toSafeJSON() } });
+};
+
+module.exports = { register, login, me };
